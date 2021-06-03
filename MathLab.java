95e8dce28e95bcb64fb2eadebd22cbfd3eee22b8
mrblakemore <35924046+mrblakemore@users.noreply.github.com>
2021-06-03 08:34:51 -0400

    Predefined Math Methods
    Teaches Math.max, Math.min, Math.pow, Math.log, Math.hypot

---
+++ MathLab.java
@@ -0,0 +1,16 @@
+public class MathLab
+{
+    public static void main(String[] args)
+    {
+        int num1 = 4;
+        int num2 = 6;
+        
+        System.out.println("Max: " + Math.max(num1, num2));
+        System.out.println("Min: " + Math.min(num1, num2));
+        System.out.println("Power: " + Math.pow(num1, num2));
+        System.out.println("Log of num1: " + Math.log(num1));
+        System.out.println("Log of num2: " + Math.log(num2));
+        System.out.println("Hypotenuse: " + Math.hypot(num1, num2));
+    }
+}
+
