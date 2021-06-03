22cacb1913ca225928b6deac1eed966642ea7ac5
mrblakemore <35924046+mrblakemore@users.noreply.github.com>
2021-06-03 08:26:26 -0400

    Expressions Lab
    Teaches Expressions, Mixed Expressions, Order of Operations, Integer Division, Remainder, Casting

---
+++ Expressions.java
@@ -0,0 +1,40 @@
+public class Expressions
+{
+    public static void main(String[] args)
+    {
+        int a = 1 + 2;
+        System.out.println("1 + 2 = " + a);
+        
+        double b = 5.2 + 3.5;
+        System.out.println("5.2 + 3.5 = " + b);       
+        
+        int c = 13 - 5;
+        System.out.println("13 - 5 = " + c);
+        
+        double d = 4.2 - 2.5;
+        System.out.println("4.2 - 2.5 = " + d);       
+        
+        double e = 3.6 / 2;
+        System.out.println("3.6 / 2 = " + e);       
+        
+        double f = 15 / 2 + 5.5;
+        System.out.println("15 / 2 + 5.5 = " + f);       
+        
+        int g = 3 / 2;
+        System.out.println("3 / 2 = " + g);
+        
+        int h = 5 % 2;
+        System.out.println("5 % 2 = " + h);
+        
+        int i = (int)(7.9) ;
+        System.out.println("(int)(7.9)  = " + i);
+        
+        int j = (int)(3.3);
+        System.out.println("(int)(3.3)  = " + j);
+        
+        int k = (int)(7.8 + (double)(15 / 2));
+        System.out.println("(int)(7.8 + (double)(15 / 2)) = " + k);
+ 
+    }
+
+}
