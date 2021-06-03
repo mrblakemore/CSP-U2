0474fbadb322003ee37f7f82091d9358447e7976
mrblakemore <35924046+mrblakemore@users.noreply.github.com>
2021-06-03 08:19:39 -0400

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,20 @@
+public class AboutMe
+{
+   public static void main(String[] args)
+   {
+        System.out.println("Hello!");
+        
+        String name = "Mr. Blakemore";
+        System.out.println("My name is " + name);
+        
+        int age = 42;
+        System.out.println("I am " + age + " years old.");
+        
+        double gpa = 3.5;
+        System.out.println("My gpa is " + gpa);
+        
+        String color = "red";
+        System.out.println("My favorite color is " + color);
+
+   }
+}
