4b8c8248a340bd0d6dc9ea2ab0fc60ed55b9fb1a
mrblakemore <35924046+mrblakemore@users.noreply.github.com>
2021-06-03 08:36:32 -0400

    Predefined String Methods
    Teaches toLowerCase(), toUpperCase(), replace(), length()

---
+++ StringLab.java
@@ -0,0 +1,19 @@
+public class StringLab
+{
+    public static void main(String[] args)
+    {
+        String phrase = "Have a nice day.";
+        
+        System.out.println(phrase);
+        System.out.println(phrase.toLowerCase());
+        System.out.println(phrase.toUpperCase());
+        System.out.println(phrase.replace('a', '@'));
+        System.out.println("Number of characters: " + phrase.length());
+    }
+}
+
+
+
+
+
+
